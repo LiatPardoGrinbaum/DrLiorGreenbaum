10dc851a3b96f7f9be5deb0b9b90d3f286532712
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2024-08-13 18:30:02 +0300

    changed tab name

--- src/pages/HomeMain.tsx
+++ src/pages/HomeMain.tsx
@@ -7,7 +7,7 @@ const HomeMain = () => {
   return (
     <Box minHeight="100vh" bgcolor="white" width="100%" sx={{ display: "flex", flexDirection: "column" }}>
       <Helmet>
-        <title>ד"ר ליאור גרינבאום ייעוץ נוירוגנטי</title>
+        <title>פרופ' ליאור גרינבאום ייעוץ נוירוגנטי</title>
         <meta name="description" content='ייעוץ נוירולוגיה נוירוגנטיקה ד"ר ליאור גרינבאום'></meta>
         <link rel="canonical" href="/" />
       </Helmet>
